6ecb78eb7d8c2a4ef53cead8bfca3ec6f7e31bd8
Eren Demirdas <120308046+erendemirdas@users.noreply.github.com>
2022-12-18 18:55:34 +0300

    Add files via upload

---
+++ artikYil/artikYil.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class artikYil {
+    public static void main(String[] args) {
+        int yil , mod1 , mod2 ,mod3;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        yil = inp.nextInt();
+
+        mod1 = yil % 4 ;
+        mod2 = yil % 100;
+        mod3 = yil % 400;
+
+        if (mod2==0){
+            if (mod3==0){
+                System.out.println(yil + " bir artık yıldır !");
+            }else {
+                System.out.println(yil + " bir artık yıl değildir !");
+            }
+        }else if(mod1 == 0){
+            System.out.println(yil + " bir artık yıldır !");
+        }else {
+            System.out.println(yil + " bir artık yıl değildir !");
+        }
+    }
+}
